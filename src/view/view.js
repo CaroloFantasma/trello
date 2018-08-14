34ba7eee2054df1d9731ff390db577d32179b00a
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-14 12:19:07 -0300

    focus

--- src/view/view.js
+++ src/view/view.js
@@ -3,7 +3,7 @@ window.addNewList = function() {
     newList.innerHTML = `
     <form>
       <div class="form-group" id="newListWrapper">
-        <input type="text" id="newListInput" placeholder="Introduzca el título de la lista...">
+        <input type="text" id="newListInput" autofocus placeholder="Introduzca el título de la lista...">
         <div>
         <button type"button" id="addingListBtn" onClick= window.addingList()>Añadir lista</button>
         <button type="button" id="closeNewListBtn" onClick="window.closeBtn()"><i class="fas fa-times"></i></button>
@@ -32,6 +32,18 @@ window.addingList = function() {
     <div>
       <button type"button" id="addingCard" onClick= window.addingAnotherList()><i class="fas fa-plus"></i>Añada una tarjeta</button>
     </div>
+  </div>
+
+  <div id="formContainer"> 
+    <form>
+      <div class="form-group" id="newListWrapper">
+        <input type="text" id="newListInput" placeholder="Introduzca el título de la lista...">
+      <div>
+        <button type"button" id="addingListBtn" onClick= window.addingList()>Añadir lista</button>
+        <button type="button" id="closeNewListBtn" onClick="window.closeBtn()"><i class="fas fa-times"></i></button>
+      </div>
+    </div>
+    </form>
   </div>
   `
 };
