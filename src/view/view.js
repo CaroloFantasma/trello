0e53030fee53dbde16066cf29d5aa721020d8c3f
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-14 09:11:12 -0300

    Agregando tarjetas

--- src/view/view.js
+++ src/view/view.js
@@ -1,24 +1,56 @@
-window.addNewList = function () {
+window.addNewList = function() {
   const newList = document.getElementById ('addListContainer');
     newList.innerHTML = `
     <form>
       <div class="form-group" id="newListWrapper">
         <input type="text" id="newListInput" placeholder="Introduzca el título de la lista...">
         <div>
-        <button type"button" id="addingListBtn" onClick= addingList()>Añadir lista</button>
+        <button type"button" id="addingListBtn" onClick= window.addingList()>Añadir lista</button>
         <button type="button" id="closeNewListBtn" onClick="window.closeBtn()"><i class="fas fa-times"></i></button>
         </div>
       </div>
     </form>
     `
-}
+};
 
-window.closeBtn = function () {
+window.closeBtn = function() {
   const closeNewList = document.getElementById ('addListContainer');
   closeNewList.innerHTML = `
   <button id="addCardBtn" onclick="window.addNewList()">
-  <i class="fas fa-plus" id="addCardIcon"></i>
-  <p id="AddListTitle">Añada una lista</p>
-</button>
+    <i class="fas fa-plus" id="addCardIcon"></i>
+    <p id="AddListTitle">Añada una lista</p>
+  </button>
   `    
-}
+};
+
+window.addingList = function() {
+  const inputAddListValue = document.getElementById('newListInput').value;
+  const addingNewList = document.getElementById ('addListContainer');
+  addingNewList.innerHTML = `
+  <div class="form-group" id="newListWrapper">
+    <p><strong>${inputAddListValue}</strong></p>
+    <div>
+      <button type"button" id="addingCard" onClick= window.addingAnotherList()><i class="fas fa-plus"></i>Añada una tarjeta</button>
+    </div>
+  </div>
+  `
+};
+
+window.addingAnotherList = function() {
+  // const inputAddListValue = document.getElementById('newListInput').value;
+  const addingAnotherList = document.getElementById ('addListContainer');
+  addingAnotherList.innerHTML = `
+  <div class="form-group" id="newListWrapper">
+    // <p><strong>${inputAddListValue}</strong></p>
+    <form>
+    <div class="form-group" id="newListWrapper">
+      <input type="text" id="newListInput" placeholder="Introduzca el título de la lista...">
+      <div>
+      <button type"button" id="addingListBtn" onClick= window.addingList()>Añadir tarjeta</button>
+      <button type="button" id="closeNewListBtn" onClick="window.closeBtn()"><i class="fas fa-times"></i></button>
+      </div>
+    </div>
+  </form>
+  </div>
+  `
+};
