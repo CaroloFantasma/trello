96a40fd7bc6a4075b6e6ec58df3dd076eafa4d14
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-13 22:22:18 -0300

    Avances

--- src/view/view.js
+++ src/view/view.js
@@ -2,10 +2,12 @@ window.addNewList = function () {
   const newList = document.getElementById ('addListContainer');
     newList.innerHTML = `
     <form>
-      <div class="form-group">
+      <div class="form-group" id="newListWrapper">
         <input type="text" id="newListInput" placeholder="Introduzca el título de la lista...">
+        <div>
         <button type"button" id="addingListBtn" onClick= addingList()>Añadir lista</button>
         <button type="button" id="closeNewListBtn" onClick="window.closeBtn()"><i class="fas fa-times"></i></button>
+        </div>
       </div>
     </form>
     `
